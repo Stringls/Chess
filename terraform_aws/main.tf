b3e571577687d432b8c1e992a45f6abb5c229b76
Artem <temasad12@gmail.com>
2022-07-01 16:09:09 +0300

    update vpc module

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -35,6 +35,19 @@ module "vpc" {
   repo_url   = var.repo_url
 
   aws_region = var.aws_region
+
+  vpc_cidr = "10.0.0.0/16"
+
+  route = [
+    {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = module.vpc.gateway_id
+      instance_id = null
+      nat_gateway_id = null
+    }
+  ]
+
+  subnet_ids = module.subnet
 }
 
 ######################################################
--- terraform_aws/modules/vpc/main.tf
+++ terraform_aws/modules/vpc/main.tf
@@ -1,5 +1,5 @@
 # Internet VPC
-resource "aws_vpc" "main" {
+resource "aws_vpc" "_" {
   cidr_block           = var.vpc_cidr
 
   enable_dns_support   = var.enable_dns_hostnames
@@ -14,8 +14,8 @@ resource "aws_vpc" "main" {
 }
 
 # Internet GW
-resource "aws_internet_gateway" "main-gw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "_" {
+  vpc_id = aws_vpc._.id
 
   tags = {
     Name      = "main"
@@ -29,12 +29,18 @@ resource "aws_internet_gateway" "main-gw" {
 # Route tables
 ####################################
 
-resource "aws_route_table" "main-public" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "_" {
+  vpc_id = aws_vpc._.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main-gw.id
+  dynamic "route" {
+    for_each = var.route
+
+    content {
+      cidr_block = route.value.cidr_block
+      gateway_id = route.value.gateway_id
+      instance_id = route.value.instance_id
+      nat_gateway_id = route.value.nat_gateway_id
+    }
   }
 
   tags = {
@@ -45,121 +51,128 @@ resource "aws_route_table" "main-public" {
   }
 }
 
-resource "aws_route_table" "main-private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main-gw.id
-  }
-
-  tags = {
-    Name      = "main-private-1"
-    Env       = var.env
-    ManagedBy = "terraform"
-    Repo      = var.repo_url
-  }
-}
+#resource "aws_route_table" "main-private" {
+#  vpc_id = aws_vpc.main.id
+#
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = aws_internet_gateway.main-gw.id
+#  }
+#
+#  tags = {
+#    Name      = "main-private-1"
+#    Env       = var.env
+#    ManagedBy = "terraform"
+#    Repo      = var.repo_url
+#  }
+#}
 
 ####################################
 # Subnets
 ####################################
 
-resource "aws_subnet" "main-public-1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "${var.aws_region}a"
+# resource "aws_subnet" "main-public-1" {
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.1.0/24"
+#   map_public_ip_on_launch = true
+#   availability_zone       = "${var.aws_region}a"
 
-  tags = {
-    Name      = "main-public-1"
-    Env       = var.env
-    ManagedBy = "terraform"
-    Repo      = var.repo_url
-  }
-}
+#   tags = {
+#     Name      = "main-public-1"
+#     Env       = var.env
+#     ManagedBy = "terraform"
+#     Repo      = var.repo_url
+#   }
+# }
 
-resource "aws_subnet" "main-public-2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "${var.aws_region}b"
+# resource "aws_subnet" "main-public-2" {
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.2.0/24"
+#   map_public_ip_on_launch = true
+#   availability_zone       = "${var.aws_region}b"
 
-  tags = {
-    Name      = "main-public-2"
-    Env       = var.env
-    ManagedBy = "terraform"
-    Repo      = var.repo_url
-  }
-}
+#   tags = {
+#     Name      = "main-public-2"
+#     Env       = var.env
+#     ManagedBy = "terraform"
+#     Repo      = var.repo_url
+#   }
+# }
 
-resource "aws_subnet" "main-private-1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.3.0/24"
-  map_public_ip_on_launch = false
-  availability_zone       = "${var.aws_region}a"
+# resource "aws_subnet" "main-private-1" {
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.3.0/24"
+#   map_public_ip_on_launch = false
+#   availability_zone       = "${var.aws_region}a"
 
-  tags = {
-    Name      = "main-private-1"
-    Env       = var.env
-    ManagedBy = "terraform"
-    Repo      = var.repo_url
-  }
-}
+#   tags = {
+#     Name      = "main-private-1"
+#     Env       = var.env
+#     ManagedBy = "terraform"
+#     Repo      = var.repo_url
+#   }
+# }
 
-resource "aws_subnet" "main-private-2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.4.0/24"
-  map_public_ip_on_launch = false
-  availability_zone       = "${var.aws_region}b"
+# resource "aws_subnet" "main-private-2" {
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.4.0/24"
+#   map_public_ip_on_launch = false
+#   availability_zone       = "${var.aws_region}b"
 
-  tags = {
-    Name      = "main-private-2"
-    Env       = var.env
-    ManagedBy = "terraform"
-    Repo      = var.repo_url
-  }
-}
+#   tags = {
+#     Name      = "main-private-2"
+#     Env       = var.env
+#     ManagedBy = "terraform"
+#     Repo      = var.repo_url
+#   }
+# }
 
 ####################################
 # route associations
 ####################################
 
+resource "aws_route_table_association" "_" {
+  count = length(var.subnet_ids)
+
+  subnet_id = element(var.subnet_ids, count.index)
+  route_table_id = aws_route_table._.id
+}
+
 # public
-resource "aws_route_table_association" "main-public-1-a" {
-  subnet_id      = aws_subnet.main-public-1.id
-  route_table_id = aws_route_table.main-public.id
-}
+# resource "aws_route_table_association" "main-public-1-a" {
+#   subnet_id      = aws_subnet.main-public-1.id
+#   route_table_id = aws_route_table.main-public.id
+# }
 
-resource "aws_route_table_association" "main-public-2-a" {
-  subnet_id      = aws_subnet.main-public-2.id
-  route_table_id = aws_route_table.main-public.id
-}
+# resource "aws_route_table_association" "main-public-2-a" {
+#   subnet_id      = aws_subnet.main-public-2.id
+#   route_table_id = aws_route_table.main-public.id
+# }
 
-# private
-resource "aws_route_table_association" "main-private-1-a" {
-  subnet_id      = aws_subnet.main-private-1.id
-  route_table_id = aws_route_table.main-private.id
-}
+# # private
+# resource "aws_route_table_association" "main-private-1-a" {
+#   subnet_id      = aws_subnet.main-private-1.id
+#   route_table_id = aws_route_table.main-private.id
+# }
 
-resource "aws_route_table_association" "main-private-2-a" {
-  subnet_id      = aws_subnet.main-private-2.id
-  route_table_id = aws_route_table.main-private.id
-}
+# resource "aws_route_table_association" "main-private-2-a" {
+#   subnet_id      = aws_subnet.main-private-2.id
+#   route_table_id = aws_route_table.main-private.id
+# }
 
 ####################################
 # nat gw
 ####################################
 
-resource "aws_eip" "nat" {
-  vpc = true
-}
+# resource "aws_eip" "nat" {
+#   vpc = true
+# }
 
-resource "aws_nat_gateway" "nat-gw" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.main-public-1.id
+# resource "aws_nat_gateway" "nat-gw" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.main-public-1.id
 
-  depends_on = [
-    aws_internet_gateway.main-gw
-  ]
-}
+#   depends_on = [
+#     aws_internet_gateway.main-gw
+#   ]
+# }
