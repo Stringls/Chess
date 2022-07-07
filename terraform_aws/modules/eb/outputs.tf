c85637106d61bd3d0c4f41b7d399fb502658e81f
vagrant <vagrant@localhost.localdomain>
2022-07-07 14:12:48 +0300

    add two more outputs

--- terraform_aws/modules/eb/outputs.tf
+++ terraform_aws/modules/eb/outputs.tf
@@ -1,3 +1,11 @@
 output "eb_security_group_id" {
   value = aws_security_group.eb_app_prod.id
 }
+
+output "app_name" {
+  value = aws_elastic_beanstalk_application.app.name
+}
+
+output "env_app" {
+  value = aws_elastic_beanstalk_environment.app-prod.name
+}
