ffc55a48236a9112721aa7138f332a28d9b0d838
vagrant <vagrant@localhost.localdomain>
2022-07-07 10:09:41 +0300

    update s3 bucket module

--- terraform_aws/modules/s3/main.tf
+++ terraform_aws/modules/s3/main.tf
@@ -1,3 +1,28 @@
-resource "aws_s3_bucket" "name" {
-  
+# cache s3 bucket
+resource "aws_s3_bucket" "codebuild-cache" {
+  bucket = "${var.identifier}-codebuild-cache-${random_string.random.result}"
+}
+
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "${var.identifier}-artifacts-${random_string.random.result}"
+}
+
+# Lifecycle configuration is available starting with AWS Provider 4.x
+resource "aws_s3_bucket_lifecycle_configuration" "artifacts-lifecycle" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  rule {
+      id = "${var.rule_id}"
+      status = "${var.lifecycle_rule_status}"
+
+      expiration {
+        days = var.lifecycle_expiration
+      }
+  }
+}
+
+resource "random_string" "random" {
+  length = 8
+  special = false
+  upper = false
 }
